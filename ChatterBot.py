d53f8cc95a97ece61110958ca6c875fd918d938a
duymnguyen9 <30640455+duymnguyen9@users.noreply.github.com>
2017-12-07 20:36:07 -0800

    updates

--- ChatterBot.py
+++ ChatterBot.py
@@ -18,7 +18,7 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 # Create a function that tweets
 def TweetOut(tweet_number):
     api.update_status(
-        "Can't stop. Won't stop. Chatting! This is Tweet #%s! UCI-Bootcamp" %
+        "HEROKU DOES NOT WORK!!!!!! #%s! UCI-Bootcamp" %
         tweet_number)
 
 
